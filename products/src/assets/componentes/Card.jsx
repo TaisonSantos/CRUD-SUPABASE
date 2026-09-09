603f214fa0fbeacd2b1faaebfbe0b4eb83128850
Tailson Moraes <tailsonfrontend@gmail.com>
2026-09-09 11:30:20 -0300

    primeiro commit

---
+++ products/src/assets/componentes/Card.jsx
@@ -0,0 +1,10 @@
+export function Card({id,nome,valor,estoque}){
+    return(
+        <div>
+            <p>id:{id}</p>
+            <p>nome:{nome}</p>
+            <p>valor:{valor}</p>
+            <p>estoque:{estoque}</p>
+        </div>
+    )
+}
